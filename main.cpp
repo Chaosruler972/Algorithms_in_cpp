f999d48a93cb58be974689c4260699d4efce5007
Chaosruler <trunks_ishter@hotmail.com>
2018-01-23 21:30:30 +0200

    Update with folders and new dps

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "UnusedImportStatement"
 #include <iostream>
 
-#include "Design Patterns/Singleton/singleton.h"
+
 
 int main() {
-    Singleton &singleton = Singleton::getInstance();
-    Singleton &singleton1 = Singleton::getInstance();
 
-    std::cout << singleton << std::endl;
-    std::cout << singleton1 << std::endl;
     return 0;
 }
+#pragma clang diagnostic pop
